144be0a502c24d5464580a9df6cad80d0da64aa4
Lilian144k <febrissy.lilian@gmail.com>
2024-04-23 10:29:05 +0200

    j'ai mis en bordeau le fond

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Formulaire de Contact</title>
-    <link rel="stylesheet" href="style_formulaire.css" />
+    <link rel="stylesheet" href="css\style_formulaire.css" />
     <link
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"/>
   </head>
