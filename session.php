0cecc6c337dc0412f47419b96320cdb775d1d9b5
yourDoudou <163140169+yourDoudou@users.noreply.github.com>
2024-04-26 11:19:57 +0200

    Update session.php

--- session.php
+++ session.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'bdd.php';
 
 // Récupérer les données des catégories depuis la base de données
@@ -10,22 +11,21 @@ if ($categories) {
     echo "Erreur lors de la récupération des catégories depuis la base de données.";
 }
 
+
 // Récupérer les données des produits depuis la base de données
 $produits = recupererProduits();
 if ($produits) {
     $_SESSION['produits'] = $produits;
 } else {
-    // Gérer l'erreur si la récupération des produits a échoué
     echo "Erreur lors de la récupération des produits depuis la base de données.";
 }
+
 
 // Récupérer les données des clients depuis la base de données
 $clients = recupererClients();
 if ($clients) {
     $_SESSION['clients'] = $clients;
 } else {
-    // Gérer l'erreur si la récupération des clients a échoué
     echo "Erreur lors de la récupération des clients depuis la base de données.";
 }
 ?>
-
